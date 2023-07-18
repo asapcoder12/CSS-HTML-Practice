21fb10a5d8c91ea75d421911bfa8e68e38d04d7c
ASAP Coder <ozchivit12@gmail.com>
2023-07-19 00:53:42 +0300

    Dental Tooth

--- Dental Tooth/js/script.js
+++ Dental Tooth/js/script.js
@@ -6,4 +6,27 @@ $(document).ready(function () {
     $('.mobile-menu-close').on('click', function() {  
         $('.mobile-menu').removeClass('is-active');
     });
+
+    //Reviews Swipert Start
+
+    const reviewsSwiper = new Swiper('.reviews-swiper', {
+        loop: true,
+        navigation: {
+          nextEl: '.reviews-swiper .swiper-button-next',
+          prevEl: '.reviews-swiper .swiper-button-prev',
+        },
+        speed: 1000
+    });
+
+    //Reviews Swipert End
+    $('.header-button').magnificPopup(
+        {
+            type:'inline',
+            removalDelay: 700,
+            callbacks: {
+                beforeOpen: function() {
+                   this.st.mainClass = this.st.el.attr('data-effect');
+                }
+            },
+        });
 });
